5fde2c37d7f77f0cb249d6a25cabf29e39a81565
AntonZharkov <19antoni88@gmail.com>
2023-05-10 13:50:18 +0300

    Add filters, ordering, sorting Add unittest for filters, ordering

--- books/books/settings.py
+++ books/books/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
--- books/store/admin.py
+++ books/store/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Book
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    pass
